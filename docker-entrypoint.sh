540c12fc7df93c3572971541c23a1b9380984b26
Konstantin Malanchev <hombit@gmail.com>
2020-08-01 10:26:50 +0300

    Update docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 cd /app
+
+echo "Waiting 10 seconds while database starts"
+sleep 10
+
+echo "Migrating database"
 python manage.py migrate
+
+echo "Starting server"
 gunicorn akb.wsgi --workers=2 --bind=0.0.0.0:80
